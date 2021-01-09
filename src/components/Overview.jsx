e5a602107f210a399d4b98afd8bef23d4631c597
Jordan <jordanleelong5@gmail.com>
2021-01-08 22:33:33 -0800

    created a static component to gice a brief overview

---
+++ src/components/Overview.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+function Overview() {
+    let smiley = ':)'
+    return (
+        <section>
+            <div>
+                <h1>React Fundamentals!</h1>
+                <p>As I work through <a href='http://EpicReact.Dev' >EpicReact</a>, a tremendous workshop created by <a href="https://twitter.com/kentcdodds">Kent C. Dodds</a> I wanted to figure out a way to solidify my learning. With some insporation from <a href='https://twitter.com/tony_venits'>Tony Przybyt</a> who came up with the idea I wanted to create my own version. Here is the first quiz, this is on React Fundamentals. Enjoy {smiley} </p>
+            </div>
+        </section>
+    )
+}
+
+export default Overview
