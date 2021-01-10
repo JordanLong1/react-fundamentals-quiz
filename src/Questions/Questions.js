b2c30a4f94223d6d68695c1f8aa730b7118945fc
Jordan <jordanleelong5@gmail.com>
2021-01-09 20:32:12 -0800

    added answers, count state, onSubmit threw alert for now until i build submission component

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,39 +1,71 @@
 import React, {useState} from 'react'; 
-import q1 from '../images/q1.png'
 
- const questionBank = [
-{question: `What does babel compile this code snippet to: `, 
-answer1: 'Answer one', 
-answer2: 'Answer Two', 
-answer3: 'Answer three', 
-answer4: 'Answer 4', 
-correct: 'answer3', 
-id: 1
-}
-]
+
+
+     const questionBank = [
+         {question: `What does babel compile this code snippet to: `, 
+             answer1: 'const container = /*#__PURE__*/ document.createElement("div",{classsName: "container" },"Welcome to the React Fundamentals Quiz!")', 
+                  answer2: 'const container =  /*#__PURE__*/ React.createElement("div",{classsName: "container", textContent: "Welcome to the React Fundamentals Quiz!" })', 
+                      answer3: `const container = /*#__PURE__*/ React.createElement("div",{classsName: "container" },"Welcome to the React Fundamentals Quiz!")`, 
+                         answer4: 'Invalid Input', 
+                              correct: 'answer3', 
+                                  id: 1
+                              }
+                        ]
 
     export default function Questions() { 
 
         const [selected, setSelected] = useState(''); 
+        const [count, setCount] = useState(0); 
+
 
         const handleChange = (e) => {
+            let correctAnswer = `const container = /*#__PURE__*/ React.createElement("div",{classsName: "container" },"Welcome to the React Fundamentals Quiz!")`
+
             setSelected(e.target.value)
+
+            if (e.target.value === correctAnswer) {
+                setCount(prevCount => prevCount + 1)
+            }
         }
-   
+
+        const handleSubmit = (e) => {
+            e.preventDefault()
+
+            alert(`you got ${count} out of 1 correct`)
+        }
+        
+        const q = `const container = <div className='container'>Welcome to the React Fundamentals Quiz</div>`
     return (
         <div className='q-container'>
          {questionBank.map(question => <div key={question.id}> 
          <div  className='question-box'>{question.question}</div>
-         <img style={{width: '40%'}} src={q1} alt=''></img>
-            <form>
-          <div> <input type='radio' className='answer-list' value={'Answer one'} onChange={handleChange} checked={selected === question.answer1} ></input>A. {question.answer1}</div>
-          <div> <input type='radio' className='answer-list' value={'Answer Two'} onChange={handleChange} checked={selected === question.answer2 }></input>  B. {question.answer2} </div>
-          <div> <input type='radio' className='answer-list' value={'Answer three'} onChange={handleChange} checked={selected === question.answer3 }></input>  C. {question.answer3}</div> 
-           <div> <input type='radio' className='answer-list' value={'Answer 4'} onChange={handleChange} checked={selected === question.answer4}></input> D. {question.answer4}</div>
+            <pre><code>{q}</code></pre>
 
+          <form onSubmit={handleSubmit}>
+
+         <div>
+            <input type='radio' className='answer-list' value={question.answer1} onChange={handleChange} checked={selected === question.answer1} ></input>
+                <pre><code>{question.answer1}</code></pre>
+         </div>
+
+          <div>
+            <input type='radio' className='answer-list' value={question.answer2} onChange={handleChange} checked={selected === question.answer2 }></input>
+                 <pre><code>{question.answer2} </code></pre>
+          </div>
+
+         <div>
+             <input type='radio' className='answer-list' value={question.answer3} onChange={handleChange} checked={selected === question.answer3 }></input>
+                 <pre><code>C. {question.answer3}</code></pre>
+         </div>
+
+        <div> 
+           <input type='radio' className='answer-list' value={question.answer4} onChange={handleChange} checked={selected === question.answer4}></input>
+               <pre><code>{question.answer4}</code></pre>
+        </div>
+
+            <button>Submit</button>
             </form>
-
-   
           </div>)}
         </div>
     )
