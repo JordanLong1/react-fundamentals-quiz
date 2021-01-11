008461b53ab80675bd95b4cc86aad1d9c02fc414
Jordan <jordanleelong5@gmail.com>
2021-01-10 21:03:44 -0800

    imported and rendered questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Overview from './components/Overview'
+import Questions from './Questions/Questions.js'
 function App() {
+
+
   return (
     <div className="App">
     <Overview />
+    <Questions />
     </div>
   );
 }
