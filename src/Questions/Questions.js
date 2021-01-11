8937dfabcf23af21629a18e26cf95879eeb7ac7e
Jordan <jordanleelong5@gmail.com>
2021-01-10 21:03:31 -0800

    removed radio button form, extracted questionBank. Going to get bulk of it working then refactor.

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,74 +1,46 @@
 import React, {useState} from 'react'; 
-
-
-
-     const questionBank = [
-         {question: `What does babel compile this code snippet to: `, 
-             answer1: 'const container = /*#__PURE__*/ document.createElement("div",{classsName: "container" },"Welcome to the React Fundamentals Quiz!")', 
-                  answer2: 'const container =  /*#__PURE__*/ React.createElement("div",{classsName: "container", textContent: "Welcome to the React Fundamentals Quiz!" })', 
-                      answer3: `const container = /*#__PURE__*/ React.createElement("div",{classsName: "container" },"Welcome to the React Fundamentals Quiz!")`, 
-                         answer4: 'Invalid Input', 
-                              correct: 'answer3', 
-                                  id: 1
-                              }
-                        ]
-
+import ListOfQuestions from './ListOfQuestions'
     export default function Questions() { 
+
+        const questionBank = ListOfQuestions()
 
         const [selected, setSelected] = useState(''); 
         const [count, setCount] = useState(0); 
 
 
-        const handleChange = (e) => {
-            let correctAnswer = `const container = /*#__PURE__*/ React.createElement("div",{classsName: "container" },"Welcome to the React Fundamentals Quiz!")`
+        const handleClick = (e) => {
+            const correctAnswer = questionBank[0].correct
 
-            setSelected(e.target.value)
+            setSelected(e.target.innerText)
 
-            if (e.target.value === correctAnswer) {
+            if (e.target.innerText === correctAnswer) {
                 setCount(prevCount => prevCount + 1)
             }
         }
 
         const handleSubmit = (e) => {
+            console.log('hello')
             e.preventDefault()
 
-            alert(`you got ${count} out of 1 correct`)
+            alert(`you got ${count} out of ${questionBank.length}`)
+            setSelected('')
         }
         
-        const q = `const container = <div className='container'>Welcome to the React Fundamentals Quiz</div>`
     return (
         <div className='q-container'>
-         {questionBank.map(question => <div key={question.id}> 
-         <div  className='question-box'>{question.question}</div>
-            <pre><code>{q}</code></pre>
-
-          <form onSubmit={handleSubmit}>
-
-         <div>
-            <input type='radio' className='answer-list' value={question.answer1} onChange={handleChange} checked={selected === question.answer1} ></input>
-                <pre><code>{question.answer1}</code></pre>
-         </div>
-
-          <div>
-            <input type='radio' className='answer-list' value={question.answer2} onChange={handleChange} checked={selected === question.answer2 }></input>
-                 <pre><code>{question.answer2} </code></pre>
-          </div>
-
-         <div>
-             <input type='radio' className='answer-list' value={question.answer3} onChange={handleChange} checked={selected === question.answer3 }></input>
-                 <pre><code>C. {question.answer3}</code></pre>
-         </div>
-
-        <div> 
-           <input type='radio' className='answer-list' value={question.answer4} onChange={handleChange} checked={selected === question.answer4}></input>
-               <pre><code>{question.answer4}</code></pre>
-        </div>
-
-            <button>Submit</button>
-            </form>
+             {questionBank.map(question => <div key={question.id}> 
+                  <div className='question-box'>{question.question} <pre><code>{question.q}</code></pre>  </div>
+                <div>
+                <ul>
+                    {question.answers.map((answer, index) => <li key={index} style={{border: 'solid 1px black'}} onClick={handleClick}>{answer}</li>)}
+                </ul>
+                </div>      
           </div>)}
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
+
+
 
 
